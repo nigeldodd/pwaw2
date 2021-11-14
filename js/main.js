673e9dd93d8b1230f4f772b69bfbb3bafe703d15
WilliamD47 <69767121+WilliamD47@users.noreply.github.com>
2021-11-14 16:26:13 +0000

    square▇

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ window.onload = () => {
                .register('./sw.js');
     }
   }
+
+  var c = document.getElementById("myCanvas");
+  var co = document.getElementById('container1');
+  var margin = 20
+  c.width = document.getElementById('fullscreen1').scrollWidth - margin
+  var ctx = c.getContext("2d");
+  ctx.fillStyle = "#FF8080";
+  ctx.fillRect(0,0,co.offsetWidth, co.offsetHeight);
